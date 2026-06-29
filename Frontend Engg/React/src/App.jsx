fe2d83d62de2ae08c7c7fa8452bed077d03aadfc
codesquadz1606-git <codesquadz1606@gmail.com>
2026-06-29 16:00:27 +0530

    React Lecture 1

---
+++ Frontend Engg/React/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import First, { First1, First2, StateComponent } from './Class 1/First'
+
+function App() {
+
+  return (
+    <>
+      {/* <First />
+      <First1/>
+      <First2/> */}
+      <StateComponent/>
+    </>
+  )
+}
+
+export default App
