1b8469efe91ca0f63f994154a627f0d4d6cf0836
codesquadz1606-git <codesquadz1606@gmail.com>
2026-06-30 10:18:58 +0530

    React Lecture 2

--- Frontend Engg/React/src/App.jsx
+++ Frontend Engg/React/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
 import First, { First1, First2, StateComponent } from './Class 1/First'
+import Second, { FormHandlingManual, FormHandlingPackage, UseEffect } from './Class 2/Second';
 
 function App() {
+  let name="Sanjay";
+  let city="Gurgaon";
 
+  let skills=["React","ES6","Node"]
   return (
     <>
       {/* <First />
       <First1/>
       <First2/> */}
-      <StateComponent/>
+      {/* <StateComponent/> */}
+      
+      {/* TO transfer data from App(parent) to Second(child) , we will share data as a attribute */}
+      {/* <Second name={name} city={city} skill={skills}/> */}
+
+      {/* <UseEffect/> */}
+      {/* <FormHandlingManual/> */}
+      <FormHandlingPackage/>
     </>
   )
 }
